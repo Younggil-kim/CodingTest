11dcd3b246732793955ffc9f6c319d449fdc4649
Younggil-kim <gom991@ajou.ac.kr>
2022-03-29 07:32:36 +0900

    22-03-29

--- Part4. 삼성기출/BaekJoon14499_주사위 굴리기.py
+++ Part4. 삼성기출/BaekJoon14499_주사위 굴리기.py
@@ -1,29 +1,13 @@
+
 n,m, x,y, k = map(int, input().split())
 
 lst = []
 for i in range(n):
     lst.append(list(map(int, input().split())))
 
-cmd = []
-cmd.append(list(map(int,input().split())))
-
-print(lst,cmd)
+cmds = list(map(int,input().split()))
 
 dice = [0,0,0,0,0,0,0]
-
-def convert(num):
-    if num == 1:
-        return 6
-    elif num == 2:
-        return 5
-    elif num == 3:
-        return 4
-    elif num == 4:
-        return 3
-    elif num == 5:
-        return 2
-    elif num == 6:
-        return 1
 
 def command(cm):
     global dice
@@ -41,7 +25,7 @@ def command(cm):
     elif cm == 3:
         dice[1] = d5
         dice[5] = d6
-        dice[3] = d2
+        dice[6] = d2
         dice[2] = d1
     elif cm == 4:
         dice[1] = d2
@@ -53,3 +37,23 @@ def command(cm):
 
 dx = [0,0,-1,1]
 dy = [1,-1,0,0]
+
+for cmd in cmds:
+    nx = x + dx[cmd-1]
+    ny = y + dy[cmd-1]
+    if nx < 0 or ny < 0 or nx >= n  or ny >= m:
+        continue
+    if lst[nx][ny] == 0:
+    # 0인경우
+        command(cmd)
+        lst[nx][ny] = dice[6]
+        print(dice[1])
+    else:
+        # 0이 아닌경우
+        command(cmd)
+        dice[6] = lst[nx][ny]
+        lst[nx][ny] = 0
+        print(dice[1])
+    x = nx
+    y = ny
+
